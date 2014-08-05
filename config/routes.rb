9573562f101dea2e4cf139063ba226a7f26f258f
shaya <shayasms128@gmail.com>
2014-08-05 16:09:00 +0300

    sadf

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   match '/contact', :to => 'pages#contact', :via => [:get] 
   match '/about', :to => 'pages#about', :via => [:get]
   match '/help', :to => 'pages#help', :via => [:get]
+  match '/signup', :to => 'users#new', :via => [:get]
   
   get 'pages/home'
  
